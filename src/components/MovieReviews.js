4c0eb3d8873cad6fcfe1d0826573342ff132a202
aaron-schuyler <asky28@gmail.com>
2020-09-11 14:39:22 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,24 @@
-// Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = ({reviews}) => {
+    function renderReviews() {
+      return reviews.map((review, index) => {
+        return (
+          <div className='review' key={index}>
+            <h2>{review.display_title}</h2>
+            <img src={review.multimedia.src} height='140' width='210' />
+            <h3>{review.headline}</h3>
+            <p>{review.summary_short}</p>
+          </div>
+        )
+    })
+  }
+
+    return (
+      <div className='review-list'>
+        {renderReviews()}
+      </div>
+    )
+}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,47 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
+const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ'
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+const API = `&api-key=${NYT_API_KEY}`
 
 // Code SearchableMovieReviewsContainer Here
+
+export default class SearchableMovieReviewsContainer extends Component {
+
+  state = {
+    reviews: [],
+    searchTerm: ''
+  }
+
+  getResults() {
+    fetch(URL + 'query=' + this.state.searchTerm + API)
+    .then(res => res.json())
+    .then(json => {
+      console.log(this.state.searchTerm)
+      this.setState({reviews: json.results})
+    })
+  }
+
+  handleSearch = (e) => {
+    e.preventDefault()
+    this.getResults()
+  }
+
+  handleInputChange = (e) => {
+    let query = e.target.value
+    this.setState({searchTerm: query})
+  }
+
+  render() {
+    return (
+      <div className='searchable-movie-reviews'>
+        <form onSubmit={this.handleSearch}>
+          <input type='text' onChange={this.handleInputChange} />
+          <input type='submit' />
+        </form>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    )
+  }
+}
