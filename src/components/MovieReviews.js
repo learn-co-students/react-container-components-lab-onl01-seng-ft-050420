bbf4f2ec35627fe4c154eba1ef312d627c1affcc
codewithirene567 <irenejinscott@gmail.com>
2020-09-12 20:57:17 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,12 @@
 // Code MovieReviews Here
+//should be stateless and functional
+
+import React from 'react';
+
+const MovieReviews = ({review}) => (
+    <div className = "review-list">
+      <h3>{review}</h3>
+    </div>
+)
+
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,28 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+    constructor() {
+        super()
+     
+        this.state = {
+          reviews: [],
+          searchTerm: ""
+        };
+      }
+
+      componentDidMount(searchTerm) {
+        fetch(URL + `&query=${searchTerm}`)
+        .then(response => response.json())
+        .then(reviews => this.setState({reviews: reviews.reviews}))
+    }
+
+      render(){
+        return (
+        <div className = "searchable-movie-reviews">
+        <MovieReviews reviews={this.state.reviews} />
+        </div>
+        )}
+}
+
+export default SearchableMovieReviewsContainer;
